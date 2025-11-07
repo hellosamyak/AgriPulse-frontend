e2769f45402b0f7441a1fec4a0f3f95cdf75aa7c
hellosamyak <jainsamyak0805@gmail.com>
2025-11-07 18:41:12 +0530

    add axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,11 @@
+// src/api/axios.js
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://agripulse-backend-9lur.onrender.com",
+const api = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://127.0.0.1:8000"
+      : "https://agripulse-backend-9lur.onrender.com",
 });
+
+export default api;
